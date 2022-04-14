c04d14fe374a71e3bb04b9dacb02b5734d448b19
Pedro Lemos <lemozxpedro@gmail.com>
2022-04-14 15:27:29 -0300

    refactor: changed user entity

--- lib/app/core/domain/entities/user.dart
+++ lib/app/core/domain/entities/user.dart
@@ -1,52 +1,16 @@
 class User {
+  final String name;
   final String email;
-  final String firstName;
-  final String gender;
-  final String lastName;
-  final String password;
-  final String cpf;
-  final String newsletter;
-  final String birthDate;
-  final String id;
 
-  const User({
-    required this.email,
-    required this.firstName,
-    required this.gender,
-    required this.lastName,
-    required this.password,
-    required this.cpf,
-    required this.newsletter,
-    required this.birthDate,
-    required this.id,
-  });
+  const User({required this.name, required this.email});
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
-    return other is User &&
-        other.email == email &&
-        other.firstName == firstName &&
-        other.gender == gender &&
-        other.lastName == lastName &&
-        other.password == password &&
-        other.cpf == cpf &&
-        other.newsletter == newsletter &&
-        other.birthDate == birthDate &&
-        other.id == id;
+    return other is User && other.name == name && other.email == email;
   }
 
   @override
-  int get hashCode {
-    return email.hashCode ^
-        firstName.hashCode ^
-        gender.hashCode ^
-        lastName.hashCode ^
-        password.hashCode ^
-        cpf.hashCode ^
-        newsletter.hashCode ^
-        birthDate.hashCode ^
-        id.hashCode;
-  }
+  int get hashCode => name.hashCode ^ email.hashCode;
 }
